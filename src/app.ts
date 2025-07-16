7820d2552a4b561837931f0583c5a59e0eed1808
Daniel Velkov <danielvelkov13@gmail.com>
2025-07-16 16:45:19 +0300

    feat: add home page view
    * feat: add hero section to index.ejs view with a 'Get started' button for sign up
    
    * style: enhance header layout with flex properties in index.ejs
    
    * feat: add link to the github repo of this project and font awesome icon for github
    
    * feat: add login and signup links to header in index.ejs
    
    * feat: add logo and title to header in index.ejs
    
    * feat: add header section with Roboto font styling to index.ejs
    
    * feat: add Roboto font to tailwind config
    
    * chore: update gitignore to exclude tailwind generated style.css file
    
        The css is generated on every change in the views so its better to exclude it.
        The file can be generated in the build step
    
    * build: add tailwind CLI build step to 'build' script
    
    * feat: serve static files from public directory and link tailwind stylesheet in index.ejs
    
    * feat: setup home controller index endpoint to render index.ejs view
    
    * feat: add livereload support for dev environment, config nodemon to track .ts and .ejs files
    
    * build: update dev script to use concurrently for nodemon and tailwind CSS build process command
    
    * feat: set up EJS view engine and configure views directory
    
    * build: add tailwind, ejs, livereload and livereload-connect dependencies
    
    * feat: implement WIP home page endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
 import express from 'express';
+import livereload from 'livereload';
+import connectLivereload from 'connect-livereload';
+import homeRouter from './routes/homeRouter';
+import path from 'path';
+import 'dotenv/config';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(['src/**/*']);
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('');
+    }, 10);
+  });
+  app.use(connectLivereload());
+}
+
+app.use(express.static('src/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join('src', 'views'));
+
+app.use('/', homeRouter);
 
 export default app;
---
+++ src/routes/homeRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as homeController from '../controllers/home';
+
+const homeRouter = Router();
+
+homeRouter.get('/', homeController.index);
+
+export default homeRouter;
